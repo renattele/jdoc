2cc539b40a88cdc8c2fe6277b3c75f3fb8d13313
renattele <renattele@yandex.com>
2025-01-18 15:59:13 +0300

    Removed UserSource

--- src/main/java/jdoc/user/domain/UserSource.java
+++
@@ -1,7 +0,0 @@
-package jdoc.user.domain;
-
-import jdoc.user.domain.change.UserChange;
-import jdoc.core.domain.source.DataSource;
-
-public interface UserSource extends DataSource<UserChange> {
-}
